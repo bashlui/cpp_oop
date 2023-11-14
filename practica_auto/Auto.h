9d51c490041b446becfe32b75541ae14b5f028a5
lui <134757904+bashlui@users.noreply.github.com>
2023-11-13 21:28:09 -0600

    Update Auto.h

--- practica_auto/Auto.h
+++ practica_auto/Auto.h
@@ -2,22 +2,19 @@
 
 class Auto{
     private:
-    float motor;
     std::string marca;
     std::string submarca;
 
     public:
     Auto();
-    Auto(float, std::string, std::string);
+    Auto(std::string, std::string);
 
     // Setters
-    void setMotor(float);
     void setMarca(std::string);
     void setSubmarca(std::string);
 
     // to_string method
     std::string to_string();
-    float getMotor();
     std::string getMarca();
     std::string getSubmarca();
 };
